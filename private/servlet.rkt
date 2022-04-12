7659688349359c74f56327bac619e580ea7c95f1
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-04-12 09:55:47 +0700

    format filenames localhost url

--- private/servlet.rkt
+++ private/servlet.rkt
@@ -5,18 +5,33 @@
 (define (start req)
   (response/xexpr `(html (head (title "serve-fs")))))
 
-(define (start-servlet params)
+(define ls '("info.rkt" "gura.jpeg" "LICENSE-MIT" "LICENSE-APACHE"))
+
+; (require rackunit)
+; (check-equal? (fmt "gura.jpeg") "http://localhost:5454/gura.jpeg")
+
+(define mock
+  (hash 'port 5454 'dir "." 'headless #t))
+
+; (define (start-servlet params)
+(define (start-servlet [params mock])
   (define servlet-path "/index.rkt")
+
+  (define (fmt filename)
+    (format "http://localhost:~a/~a" (hash-ref params 'port) filename))
+
   (define (command-line-mode-info)
-    (displayln (format "http://localhost:~a~a" (hash-ref params 'port) servlet-path))
-    (displayln "Stop this program at any time to terminate the Web Server."))
+    (map displayln (map fmt ls))
+    (displayln "\nStop this program at any time to terminate the Web Server."))
 
   (if (hash-ref params 'headless) (command-line-mode-info) void)
 
   (serve/servlet start
                  #:port (hash-ref params 'port)
-                 #:servlet-path servlet-path
+                 #:servlet-path "/info.rkt"
                  #:extra-files-paths (list (build-path (hash-ref params 'dir)))
-                 #:command-line? (hash-ref params 'headless)))
+                 #:command-line? (hash-ref params 'headless))
+  )
 
 (provide start-servlet)
+; (start-servlet)
