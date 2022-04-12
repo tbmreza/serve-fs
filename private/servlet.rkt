4171350078671df0b4124328e8d9c34094094585
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-04-12 10:27:18 +0700

    list filenames for command line mode

--- private/servlet.rkt
+++ private/servlet.rkt
@@ -5,7 +5,10 @@
 (define (start req)
   (response/xexpr `(html (head (title "serve-fs")))))
 
-(define ls '("info.rkt" "gura.jpeg" "LICENSE-MIT" "LICENSE-APACHE"))
+; TODO
+; (define (handle-ws ...))
+(define filenames
+  (filter file-exists? (map path->string (directory-list))))
 
 ; (require rackunit)
 ; (check-equal? (fmt "gura.jpeg") "http://localhost:5454/gura.jpeg")
@@ -21,17 +24,16 @@
     (format "http://localhost:~a/~a" (hash-ref params 'port) filename))
 
   (define (command-line-mode-info)
-    (map displayln (map fmt ls))
+    (map displayln (map fmt filenames))
     (displayln "\nStop this program at any time to terminate the Web Server."))
 
   (if (hash-ref params 'headless) (command-line-mode-info) void)
 
   (serve/servlet start
                  #:port (hash-ref params 'port)
-                 #:servlet-path "/info.rkt"
+                 #:servlet-path "/"
                  #:extra-files-paths (list (build-path (hash-ref params 'dir)))
                  #:command-line? (hash-ref params 'headless))
   )
 
 (provide start-servlet)
-; (start-servlet)
