79248e821d3857c2c911b3ef28c415fe9fb018fa
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-04-13 11:01:53 +0700

    use struct for params

--- private/servlet.rkt
+++ private/servlet.rkt
@@ -12,22 +12,24 @@
   (define not-folder-names (filter file-exists? (map path->string (directory-list))))
   (map handle-ws not-folder-names))
 
+(struct params (port dir headless?))
 (define (start-servlet params)
   (define servlet-path "/index.rkt")
 
   (define (fmt filename)
-    (format "http://localhost:~a/~a" (hash-ref params 'port) filename))
+    (format "http://localhost:~a/~a" (params-port params) filename))
 
   (define (command-line-mode-info)
     (map displayln (map fmt (filenames)))
     (displayln "\nStop this program at any time to terminate the Web Server."))
 
-  (if (hash-ref params 'headless) (command-line-mode-info) void)
+  (if (params-headless? params) (command-line-mode-info) void)
 
   (serve/servlet start
-                 #:port (hash-ref params 'port)
+                 #:port (params-port params)
                  #:servlet-path "/"
-                 #:extra-files-paths (list (build-path (hash-ref params 'dir)))
-                 #:command-line? (hash-ref params 'headless)))
+                 #:extra-files-paths (list (build-path (params-dir params)))
+                 #:command-line? (params-headless? params)))
 
-(provide start-servlet)
+(provide start-servlet
+         params)
--- serve-fs.rkt
+++ serve-fs.rkt
@@ -8,12 +8,11 @@
 (define fs-dir (box "."))
 (define command-line-mode (box #f))
 
-(command-line
- #:program "serve-fs"
- #:once-each ; Each independent, but specified at most once.
- [("--port" "-p") port "Specify port number" (set-box! localhost-port port)]
- [("--dir" "-d") dir "Specify directory to serve" (set-box! fs-dir dir)]
- [("--headless" "-s") "Do not open a browser" (set-box! command-line-mode #t)]
- #:args ()
- (start-servlet
-  (hash 'port (unbox localhost-port) 'dir (unbox fs-dir) 'headless (unbox command-line-mode))))
+(command-line #:program "serve-fs"
+              #:once-each ; Each independent, but specified at most once.
+              [("--port" "-p") port "Specify port number" (set-box! localhost-port port)]
+              [("--dir" "-d") dir "Specify directory to serve" (set-box! fs-dir dir)]
+              [("--headless" "-s") "Do not open a browser" (set-box! command-line-mode #t)]
+              #:args ()
+              (start-servlet
+               (params (unbox localhost-port) (unbox fs-dir) (unbox command-line-mode))))
